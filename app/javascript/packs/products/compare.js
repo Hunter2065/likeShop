3d840caefd18ffcd4aeece8c89b4c89d2c84ec08
Ivan <vanuha277@gmail.com>
2021-07-23 13:23:59 +0300

    refactoring

--- app/javascript/packs/products/compare.js
+++ app/javascript/packs/products/compare.js
@@ -1,3 +1,5 @@
+import { getCookie } from '../shared/cookies_manager'
+
 let products = document.getElementsByClassName('compare_button');
 let link_to_compare = document.getElementById('link_to_compare');
 let localCookies = getCookie('compareItems');
@@ -5,21 +7,11 @@ let parsedCookie = localCookies ? JSON.parse(localCookies) : [];
 let arrayOfIds = parsedCookie;
 
 let each = (elems, fn) => {
-  var i = -1;
+  let i = -1;
   while(++i < elems.length){
     fn(elems[i])
   }
 }
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-link_to_compare.addEventListener('click', event => {
-  document.cookie = `compareItems=[${arrayOfIds}];SameSite=Lax`
-});
 
 each(products, product => {
   if (!arrayOfIds.includes(parseInt(product.getAttribute('data-attribute-id')))) {
@@ -37,5 +29,6 @@ each(products, product => {
       arrayOfIds.splice(index, 1);
       product.classList.remove('selected_compare');
     }
+    document.cookie = `compareItems=[${arrayOfIds}];SameSite=Lax`;
   });
 });
